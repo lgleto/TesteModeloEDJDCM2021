003ba937bd95e35ef0c03f981b7e716afdc37a0e
Lourenço Gomes <leto.lg@gmail.com>
2020-11-18 12:38:03 +0000

    - menu

--- app/src/main/java/ipca/temperaturas/a000000/MainActivity.kt
+++ app/src/main/java/ipca/temperaturas/a000000/MainActivity.kt
@@ -3,11 +3,11 @@ package ipca.temperaturas.a000000
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -29,14 +29,37 @@ class MainActivity : AppCompatActivity() {
         tempRegistList.add(TempRegist("Famalicão", Date(), 16.4))
         tempRegistList.add(TempRegist("Guimamrães", Date(), 14.4))
 
-
         // Reposta 3
         listViewTemp = findViewById(R.id.listViewTemp)
         adapter = TempAdapter()
         listViewTemp.adapter = adapter
 
+    }
 
+    // resposta 7
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item_media -> {
+                Toast.makeText(this, "Temperatura media:${media()}", Toast.LENGTH_LONG).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun media() : Double{
+        if (tempRegistList.size < 1) return 0.0
+        var sum = 0.0
+        tempRegistList.forEach {
+            sum += it.temperature?:0.0
+        }
+        return sum/tempRegistList.size
     }
 
     // Reposta 3
@@ -57,10 +80,12 @@ class MainActivity : AppCompatActivity() {
             val rowView = layoutInflater.inflate(R.layout.row_temp, viewGroup, false)
             val textViewLocalName = rowView.findViewById<TextView>(R.id.textViewLocalName)
             val textViewTemperature = rowView.findViewById<TextView>(R.id.textViewTemperature)
+            val buttonRemove = rowView.findViewById<Button>(R.id.buttonRemove)
 
             textViewLocalName.text = tempRegistList[position].localName
             textViewTemperature.text = String.format("%.02f",tempRegistList[position].temperature)
 
+            // resposta 5
             rowView.isClickable = true
             rowView.setOnClickListener {
                 val intent = Intent (this@MainActivity, TempDetailActivity::class.java)
@@ -68,6 +93,12 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(TempDetailActivity.DATE         , tempRegistList[position].date?.time )
                 intent.putExtra(TempDetailActivity.TEMPERATURE  , tempRegistList[position].temperature)
                 startActivity(intent)
+            }
+
+            //resposta 6
+            buttonRemove.setOnClickListener {
+                tempRegistList.removeAt(position)
+                adapter.notifyDataSetChanged()
             }
 
             return rowView
--- app/src/main/java/ipca/temperaturas/a000000/TempDetailActivity.kt
+++ app/src/main/java/ipca/temperaturas/a000000/TempDetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import java.util.*
 
+// resposta 4
 class TempDetailActivity : AppCompatActivity() {
 
 
